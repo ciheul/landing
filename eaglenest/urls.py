388b9bd5c97f4a56c512cf935cc6277059c32bde
Winnu Ayi Satria <winnuayi@gmail.com>
2017-11-22 13:58:03 +0700

    add simple tci

--- eaglenest/urls.py
+++ eaglenest/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     url(r'^dashboard/$', views.DashboardView.as_view(), name='dashboard'),
+    url(r'^component/tci/$', views.ComponentTciListView.as_view(), name='component-tci-list'),
+    url(r'^component/tci/1/$', views.ComponentTciView.as_view(), name='component-tci'),
+    url(r'^component/oc/$', views.ComponentOcView.as_view(), name='component-oc'),
     url(r'^$', views.DashboardView.as_view()),
 ]
